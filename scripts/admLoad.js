567ad7284bc06f64ca9e6af191df1ace6045998e
Thiago <thiago368orjorge@gmail.com>
2024-10-02 12:35:43 -0300

    checkmain add adm

--- scripts/admLoad.js
+++ scripts/admLoad.js
@@ -1,14 +1,12 @@
 import checkMain from './home.js'
 
 // Executa ao carregar a página
-function loadpage () {
-    const token = localStorage.getItem('token');
+window.onload = function () {
 
+    const token = localStorage.getItem('token');
     checkMain(token)
 
 };
-
-document.addEventListener('DOMContentLoaded', loadpage())
 
 
 
