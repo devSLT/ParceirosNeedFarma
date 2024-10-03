a4ed7367ea102272155411420a82f137ad390b67
Thiago <thiago368orjorge@gmail.com>
2024-10-02 21:17:20 -0300

    Add data adm

--- scripts/login.js
+++ scripts/login.js
@@ -3,7 +3,18 @@ function login() {
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    const rememberme = document.getElementById('rememberme').checked;
 
+    // Verifica se o email e a senha foram preenchidos
+    if (email && password) {
+        if (rememberme) {
+            // Salva o email no localStorage
+            localStorage.setItem('email', email);
+        } else {
+            // Remove o email do localStorage se "Lembrar de mim" não estiver marcado
+            localStorage.removeItem('email');
+        }
+    }
 
     const data = {
         email,
@@ -38,5 +49,10 @@ function login() {
 
 }
 
-
-
+window.onload = function () {
+    const savedEmail = localStorage.getItem('email');
+    if (savedEmail) {
+        document.getElementById('email').value = savedEmail;
+        document.getElementById('rememberme').checked = true;
+    }
+}
