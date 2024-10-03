5734b7123165cabec10af2961d01e1ab3cf90660
Ya1Boi <ian.mourao69@gmail.com>
2024-10-02 21:26:32 -0300

    data funfante

--- scripts/adminShow.js
+++ scripts/adminShow.js
@@ -233,31 +233,6 @@ async function handleUndeny(id) {
     }
 }
 
-// Call the function to populate the table on page load
-//window.onload = populateTable;
-
-$(document).ready(function() {
-    function updateDayText() {
-        // Array com os dias da semana
-        const daysOfWeek = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-        
-        // Obter a data atual
-        const today = new Date();
-        const dayOfWeek = daysOfWeek[today.getDay()];
-        const day = today.getDate();
-        const month = today.toLocaleString('pt-BR', { month: 'long' });
-
-        // Montar o texto formatado
-        const formattedText = `<i><i class="bi bi-brightness-high"></i> ${dayOfWeek}, ${day} de ${month}</i>`;
-
-        // Atualizar o texto do elemento com id "dayText"
-        $('#dayText').html(formattedText);
-    }
-
-    // Chamar a função para atualizar o texto
-    updateDayText();
-});
-
 
 // Executa ao carregar a página
 window.onload = function () {
