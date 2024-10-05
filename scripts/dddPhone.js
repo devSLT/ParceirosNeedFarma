873080901731ab60aa92f1435da753da72a4f6da
Ya1Boi <ian.mourao69@gmail.com>
2024-10-05 17:21:15 -0300

    formatação texto cep cpf cnpj

--- scripts/dddPhone.js
+++ scripts/dddPhone.js
@@ -10,3 +10,42 @@ $(document).ready(function() {
 
     $('.sp_celphones').mask(SPMaskBehavior, spOptions);
 });
+
+$(document).ready(function() {
+    var SPMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 13 ? '000.000.000-00' : '000.000.000-00';
+        },
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+
+    $('.sp_cpf').mask(SPMaskBehavior, spOptions);
+});
+
+$(document).ready(function() {
+    var SPMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 13 ? '00.000.000/0000-00' : '00.000.000/0000-00';
+        },
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+
+    $('.sp_cnpj').mask(SPMaskBehavior, spOptions);
+});
+
+$(document).ready(function() {
+    var SPMaskBehavior = function(val) {
+            return val.replace(/\D/g, '').length === 13 ? '00000-000' : '00000-000';
+        },
+        spOptions = {
+            onKeyPress: function(val, e, field, options) {
+                field.mask(SPMaskBehavior.apply({}, arguments), options);
+            }
+        };
+
+    $('.sp_cep').mask(SPMaskBehavior, spOptions);
+});
