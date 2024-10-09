d9cb85fba9d0e6638f395ba5ec72fee4bf7218c4
Ya1Boi <thiago368orjorge@gmail.com>
2024-10-09 08:50:00 -0300

    admin negoco

--- scripts/home.js
+++ scripts/home.js
@@ -127,7 +127,7 @@ window.onload = function () {
                     //http://127.0.0.1:5501/pages/paineldecontrole/paineldecontrole.html
                     break;
                 default:
-                    cHeader.innerHTML = `<li><a class="dropdown-item" href="https://parceiros-need-farma.vercel.app/pages/paineldecontrole/paineldecontrole.html">Painel de Controle</a></li>`;
+                    cHeader.innerHTML = `<li><a class="dropdown-item" href="https://parceiros-need-farma.vercel.app/pages/adm/adminhome.html">Painel de Controle</a></li>`;
                     break;
             }
         })
